20ce4ab6416091a5d4585805b90acd14d7847d2c
rosevilleage <wkdcks125@naver.com>
2023-08-04 14:46:39 +0900

    design: browserResizer 크기 변경

--- src/components/Browser/config.ts
+++ src/components/Browser/config.ts
@@ -5,14 +5,14 @@ const MIN_W = 500;
 const MIN_H = 300;
 const DESKTOP_MT = 32; // 임시값 DesktopHeader height 으로 대체
 const RESIZERSTYLE = {
-  nw: "w-2.5 h-2.5 -top-1 -left-1 cursor-nw-resize",
-  ne: "w-2.5 h-2.5 -top-1 -right-1 cursor-ne-resize",
-  se: "w-2.5 h-2.5 -bottom-1 -right-1 cursor-se-resize",
-  sw: "w-2.5 h-2.5 -bottom-1 -left-1 cursor-sw-resize",
-  n: "h-1.5 -top-1 left-1.5 right-1.5 cursor-n-resize",
-  e: "w-1.5 top-1.5 bottom-1.5 -right-1 cursor-e-resize",
-  s: "h-1.5 -bottom-1 left-1.5 right-1.5 cursor-s-resize",
-  w: "w-1.5 top-1.5 bottom-1.5 -left-1 cursor-w-resize",
+  nw: "w-3 h-3 -top-1 -left-1 cursor-nw-resize",
+  ne: "w-3 h-3 -top-1 -right-1 cursor-ne-resize",
+  se: "w-3 h-3 -bottom-1 -right-1 cursor-se-resize",
+  sw: "w-3 h-3 -bottom-1 -left-1 cursor-sw-resize",
+  n: "h-2 -top-1 left-1.5 right-1.5 cursor-n-resize",
+  e: "w-2 top-1.5 bottom-1.5 -right-1 cursor-e-resize",
+  s: "h-2 -bottom-1 left-1.5 right-1.5 cursor-s-resize",
+  w: "w-2 top-1.5 bottom-1.5 -left-1 cursor-w-resize",
 };
 
 type Direction = "nw" | "ne" | "se" | "sw" | "n" | "e" | "s" | "w";
