c988d1bff0984c014254eb240f6cd122a261604a
rosevilleage <wkdcks125@naver.com>
2023-08-07 21:16:39 +0900

    remove: browserConfigSlice 삭제

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import browserConfig from "./slices/browserConfigSlice";
 import openApp from "./slices/openAppSlice";
 import hiddenApp from "./slices/hiddenAppSlice";
 export const store = configureStore({
   reducer: {
-    browserConfig: browserConfig,
     isOpenApp: openApp,
     isHiddenApp: hiddenApp,
   },
