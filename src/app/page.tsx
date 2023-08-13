90a9c84e5dd30005bd595640d1e6ff8540b5bf2e
rosevilleage <wkdcks125@naver.com>
2023-08-13 21:56:56 +0900

    remove: storybook 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Browser from "@/components/Browser/Browser";
 import DesktopBoundary from "@/components/desktop/DesktopBoundary";
 import SpinCircle from "@/components/desktop/symbol/SpinCircle";
-import Doc from "@/components/doc/Doc";
 
 export default function Home() {
   return (
