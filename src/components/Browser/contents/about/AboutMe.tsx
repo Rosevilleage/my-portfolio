1d3c34429e00eb296c2da9c66d3cc7f895000c67
rosevilleage <wkdcks125@naver.com>
2023-08-13 21:29:50 +0900

    refactor: about education background 요소 추가

--- src/components/Browser/contents/about/AboutMe.tsx
+++ src/components/Browser/contents/about/AboutMe.tsx
@@ -1,9 +1,11 @@
 import ContactBox from "./ContactBox";
+import Education from "./Education";
 import SkillsBox from "./SkillsBox";
 interface AboutContentProps {
   data: {
     name: string;
     introduction: string;
+    background: string;
     contact: {
       email: string;
       phone: string;
@@ -11,12 +13,17 @@ interface AboutContentProps {
       blog: string;
     };
     skill: string[];
+    education: {
+      ago: string;
+      name: string;
+      description: string[];
+    }[];
   };
 }
 export default function AboutMe({ data }: AboutContentProps) {
   return (
     <>
-      <div className="h-full">
+      <div className="h-full ">
         <div className="flex flex-col items-center justify-center w-full h-60 bg-cyan-950 text-slate-200">
           <div className="mx-auto ">
             <h1 className="text-5xl font-semibold ">{data.name}</h1>
@@ -26,8 +33,16 @@ export default function AboutMe({ data }: AboutContentProps) {
           </div>
         </div>
         <div className="p-8 ">
+          <div>
+            {data.background.split("\n").map((text) => (
+              <p key={text} className="mx-auto w-max">
+                {text}
+              </p>
+            ))}
+          </div>
           <ContactBox contact={data.contact} />
           <SkillsBox skills={data.skill} />
+          <Education education={data.education} />
         </div>
       </div>
     </>
---
+++ src/components/Browser/contents/about/Education.tsx
@@ -0,0 +1,38 @@
+interface EducationProps {
+  education: {
+    ago: string;
+    name: string;
+    description: string[];
+  }[];
+}
+
+export default function Education({ education }: EducationProps) {
+  return (
+    <div className="max-w-[1000px] w-full mx-auto mt-8">
+      <h2 className="mb-8 text-3xl text-center">Education</h2>
+      <div className="grid gap-5 grid-cols-[repeat(auto-fill,minmax(345px,1fr))] font-medium text-md">
+        {education.map((log) => (
+          <div
+            key={log.name}
+            className="flex mb-5 rounded-lg bg-cyan-700 h-[121px] shadow-gray-300 shadow-lg"
+          >
+            <div className="flex flex-col items-center justify-center p-2 text-white">
+              {log.ago.split("\n").map((time) => (
+                <p key={time}>{time}</p>
+              ))}
+            </div>
+            <ul className="w-full h-full list-disc list-inside rounded-md shadow-[0 0 15px rgba(0,0,0,0.5)] bg-slate-50">
+              <p className="p-2 text-center">{log.name}</p>
+              <div className="w-full h-0.5 bg-cyan-700"></div>
+              {log.description.map((li) => (
+                <li key={li} className="p-2 ml-2">
+                  {li}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
