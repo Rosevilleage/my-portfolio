2250f71544acc6790d3199b0b25a0a4fe5f15c00
rosevilleage <wkdcks125@naver.com>
2023-08-13 23:30:59 +0900

    refactor: about open state 초기값 원복

--- src/redux/slices/openAppSlice.ts
+++ src/redux/slices/openAppSlice.ts
@@ -10,7 +10,7 @@ export interface AppState {
 export type AppTitle = "about" | "cocktail" | "todo" | "portfolio";
 
 const initialState = {
-  about: true,
+  about: false,
   cocktail: false,
   todo: false,
   portfolio: false,
