3cc4db0f7dc6561f82593e2d37aa807550a99b3c
rosevilleage <a65083557@gmail.com>
2023-08-28 10:38:10 +0900

    refactor: add resizer, mover touch event

--- src/utills/dragMouseDown.ts
+++ src/utills/dragMouseDown.ts
@@ -4,22 +4,53 @@ export default function dragMouseDown(
   onDrag: (intervalX: number, intervalY: number) => void,
   stopPropagation?: boolean
 ) {
-  return {
-    onMouseDown: (e: React.MouseEvent<Element, MouseEvent>) => {
-      if (stopPropagation) e.stopPropagation();
+  if (
+    typeof window !== "undefined" &&
+    window.matchMedia("(hover: none) and (pointer: coarse)").matches
+  ) {
+    return {
+      onTouchStart: (touchEvent: React.TouchEvent<HTMLDivElement>) => {
+        if (stopPropagation) touchEvent.stopPropagation();
+        const touchMoveHandler = (moveEvent: TouchEvent) => {
+          if (moveEvent.cancelable) moveEvent.preventDefault();
 
-      const mouseMoveHandler = (mouseE: MouseEvent) => {
-        const intervalX = mouseE.screenX - e.screenX;
-        const intervalY = mouseE.screenY - e.screenY;
-        onDrag(intervalX, intervalY);
-      };
+          const intervalX =
+            moveEvent.touches[0].screenX - touchEvent.touches[0].screenX;
+          const intervalY =
+            moveEvent.touches[0].screenY - touchEvent.touches[0].screenY;
+          onDrag(intervalX, intervalY);
+        };
 
-      const mouseUpHandler = () => {
-        document.removeEventListener("mousemove", mouseMoveHandler);
-      };
+        const touchEndHandler = () => {
+          document.removeEventListener("touchmove", touchMoveHandler);
+        };
 
-      document.addEventListener("mousemove", mouseMoveHandler);
-      document.addEventListener("mouseup", mouseUpHandler, { once: true });
-    },
-  };
+        document.addEventListener("touchmove", touchMoveHandler, {
+          passive: false,
+        });
+        document.addEventListener("touchend", touchEndHandler, {
+          once: true,
+        });
+      },
+    };
+  } else {
+    return {
+      onMouseDown: (e: React.MouseEvent<Element, MouseEvent>) => {
+        if (stopPropagation) e.stopPropagation();
+
+        const mouseMoveHandler = (mouseE: MouseEvent) => {
+          const intervalX = mouseE.screenX - e.screenX;
+          const intervalY = mouseE.screenY - e.screenY;
+          onDrag(intervalX, intervalY);
+        };
+
+        const mouseUpHandler = () => {
+          document.removeEventListener("mousemove", mouseMoveHandler);
+        };
+
+        document.addEventListener("mousemove", mouseMoveHandler);
+        document.addEventListener("mouseup", mouseUpHandler, { once: true });
+      },
+    };
+  }
 }
