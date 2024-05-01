7810a603274d0dae773318a2907d6fcc3134e0e8
rosevilleage <chanhee.frontend@gmail.com>
2024-05-01 19:17:16 +0900

    style : fity -> fitrace

--- src/components/desktop/config.ts
+++ src/components/desktop/config.ts
@@ -1,5 +1,6 @@
 const CONTENTS = {
   about: false,
+  fitrace: false,
   cocktail: false,
   todo: false,
   portfolio: false,
@@ -7,7 +8,7 @@ const CONTENTS = {
 
 const CONTENTS_LENG = 4;
 
-const APPList = ["about", "cocktail", "todo", "portfolio"] as const;
+const APPList = ["about", "fitrace", "cocktail", "todo", "portfolio"] as const;
 
 export { CONTENTS, CONTENTS_LENG, APPList };
 
